08ed85726b3beba9c3add352477dfa41f7db30d9
neutralradiance <williamluke@me.com>
2020-10-14 17:50:56 -0500

    Move Protocols folder

---
+++ Sources/Table/Protocols/ListView.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//
+//
+//  Created by neutralradiance on 10/9/20.
+//
+
+import SwiftUI
+
+public protocol ListView: View {
+    var content: TableContent { get set }
+    var axis: Axis.Set { get set }
+    var showsIndicators: Bool { get set }
+}
